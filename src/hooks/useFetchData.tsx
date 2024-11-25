1a10f6eaadb03f16f0edb49b1c66efdd4cf617f2
Shakhzod <forworkingappsinmylaptop@gmail.com>
2024-11-25 23:53:12 +0500

    fix cors

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,25 +1,27 @@
-import axios from "axios"
-import { Base_URL } from "../api"
-import { useToastify } from "."
+import axios from "axios";
+import { Base_URL } from "../api";
+import { useToastify } from ".";
+import { axiosInstance } from "../api/Base_URL";
 
 const useFetchData = (url: string) => {
-    let test = url[0] !== '/'
-    const { toastify } = useToastify()
+  let test = url[0] !== "/";
+  const { toastify } = useToastify();
 
-    const fetchdata = async () => {
-        try {
-            const req = await axios.get(`${Base_URL}${test ? '/' : ''}${url}`)
-            return req.data
-        } 
-        catch (error) {
-            if (axios.isAxiosError(error)) {
-                toastify(error.message, "error");
-            } else {
-                toastify("An unknown error occurred", "error");
-            }
-        }
+  const fetchdata = async () => {
+    try {
+      const req = await axiosInstance.get(
+        `${Base_URL}${test ? "/" : ""}${url}`
+      );
+      return req.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toastify(error.message, "error");
+      } else {
+        toastify("An unknown error occurred", "error");
+      }
     }
-    return { fetchdata }
-}
+  };
+  return { fetchdata };
+};
 
-export default useFetchData
+export default useFetchData;

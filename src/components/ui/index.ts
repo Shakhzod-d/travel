7d5af96b262c0003a2a54a38eb5a3133f4d1493b
Shakhzod <forworkingappsinmylaptop@gmail.com>
2025-04-04 20:49:35 +0500

    fix export naming

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,22 +1,19 @@
+export { default as Cart } from "./Cart";
+export { default as OpinionCart } from "./OpinionCart";
+export { default as ReasonCart } from "./ReasonCart";
+export { default as Steps } from "./steps";
+export { default as Overlay } from "./Overlay";
+export { default as Modal } from "./modal";
+export { default as BookingModal } from "./bookingModal";
+export { default as TourBooking } from "./tourBooking";
+export { default as Map } from "./Map";
+export { default as ServiceCart } from "./serviceCart";
+export { default as Loading } from "./loader";
+export { default as LocaleSelector } from "./locale-selector";
+export { default as ScrollButton } from "./scrollButton";
 
-export { default as Cart } from './cart'
-export { default as OpinionCart } from './opinionCart'
-export { default as ReasonCart } from './reasonCart'
-export { default as Steps } from './steps'
-export { default as Overlay } from './overlay'
-export { default as Modal } from './modal'
-export { default as BookingModal } from './bookingModal'
-export { default as TourBooking } from './tourBooking'
-export { default as Map } from './map'
-export { default as ServiceCart } from './serviceCart'
-export { default as Loading } from './loader'
-export { default as LocaleSelector } from './locale-selector'
-export { default as ScrollButton } from './scrollButton'
-
-
-export { Container } from "./container";    
+export { Container } from "./container";
 export * from "./button";
 export * from "./footer";
 export * from "./rating";
 export * from "./review-card";
-

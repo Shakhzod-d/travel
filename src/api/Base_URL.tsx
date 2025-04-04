7c4476a8b7d2e160531f43141fbee0f081943c94
Shakhzod <forworkingappsinmylaptop@gmail.com>
2025-04-04 21:20:14 +0500

    fix base url

--- src/api/Base_URL.tsx
+++ src/api/Base_URL.tsx
@@ -1,4 +1,3 @@
-const Base_URL: string = "http://api.sunny-orient.uz" 
+const Base_URL: string = "https://api.sunny-orient.uz";
 
-export default Base_URL 
-// http://62.146.182.21
+export default Base_URL;
